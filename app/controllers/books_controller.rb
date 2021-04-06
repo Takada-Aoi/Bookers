555299dbc59d503eb64254a2c0a77c061bdf4e80
EC2 Default User <ec2-user@ip-172-31-16-119.ec2.internal>
2021-04-06 06:32:44 +0000

    [Add]8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
     if @book.save
     redirect_to book_path(@book), notice: "successfully"
     else
+    @books=Book.all
     render :index
     end
   end
